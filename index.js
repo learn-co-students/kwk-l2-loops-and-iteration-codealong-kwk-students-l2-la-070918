66eac4dad79752013f28bc93ed18c5190a3ba7a7
ameliebarrero@icloud.com <ameliebarrero@icloud.com>
2018-07-12 21:48:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(employees){
+  for (var count = 0; count<employees.length; count++){
+    console.log("Welcome " +employees[count]+"! You are employee #"+(count+1)+".");
+
+  }
+  return employees;
+}
+function tailsNeverFails(){
+  var tails=0;
+  while(Math.random() >= 0.5){
+    tails++;
+  }
+  return "You got " + tails + " tails in a row!";
+}
